4e9b9ec845e010fafabecdc362c49a80fb9c8e34
rui-21 <reisu6235@gmail.com>
2021-04-12 13:01:11 +0000

    [Start]3-8

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
+    p @blogs
   end
 
   def show
@@ -15,19 +16,19 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(params[blog.id])
+    redirect_to blog_path(blog.id)
   end
 
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
